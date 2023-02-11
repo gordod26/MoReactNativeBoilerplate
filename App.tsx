f8a2d1cb3bffb0ba6a9c6fedd74fb7743fe35468
David <david.gordon266@gmail.com>
2023-02-11 17:07:30 -0600

    initial commit

---
+++ App.tsx
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import {Box, NativeBaseProvider} from 'native-base';
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {Provider} from 'react-redux';
+import store from './store';
+
+function App(): JSX.Element {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <NativeBaseProvider>
+          <Box>Hello World</Box>
+        </NativeBaseProvider>
+      </NavigationContainer>
+    </Provider>
+  );
+}
+
+export default App;
